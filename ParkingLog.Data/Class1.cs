855c8dfbf8ea71950334402c9289466dffe945d6
zafarjcp <zafarjcp@gmail.com>
2021-04-04 22:56:28 +0500

    Add project files.

---
+++ ParkingLog.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ParkingLog.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ ParkingLog.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ParkingLog.Services
+{
+    public class Class1
+    {
+    }
+}
